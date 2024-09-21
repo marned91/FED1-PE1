3a52522918a1e802105cd7933c740f6e2b3beda9
marned91 <152280051+marned91@users.noreply.github.com>
2024-09-21 16:16:19 +0200

    Update create.mjs

--- javascript/posts/create.mjs
+++ javascript/posts/create.mjs
@@ -58,7 +58,9 @@ async function createBlogPost(postData, accessToken) {
 
     successAlertUser(
       "Blog post was created successfully",
-      window.location.href = "/account/dashboard.html"
+      () => {
+        window.location.href = "/account/dashboard.html"
+      }
     );
   } catch (error) {
     console.error("Error creating blog post", error);
