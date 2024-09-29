8162febeaaa21a692288aba1e0b44299129c0aa3
marned91 <152280051+marned91@users.noreply.github.com>
2024-09-29 16:55:31 +0200

    Fixed edit alt text

--- javascript/posts/update.mjs
+++ javascript/posts/update.mjs
@@ -6,6 +6,7 @@ import { successAlertUser } from "../utils/alertUser.mjs";
 
 handleGreetAndLogout();
 
+  //I initially had error handling for when altText was more than 120 characters, but I struggled a lot to get it to work. It prompted me with Error code 400 no matter how I tried to implement it. Therefore I decided to add information about the maximum limit in the form, and make it optional as I was running out of time. If alt text is not added, alt text will be set to blog post title//
 let originalAltText = "";
 let originalImageUrl = "";
 
